963a81b341c7aa8ed80439039331d6f623ad91fb
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-03-05 20:16:24 -0500

    Created the Signalis Code Bank

---
+++ SignalisCodeBank.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using System.IO;
+using UnityEngine;
+using UnityEngine.IO;
+using UnityEngine.SceneManagement;
+
+namespace VSLSignalisCodeBank
+{
+    public class SignalisCodeBank
+    {
+        public static Material TextureFind(GameObject desiredObject)
+        {
+            //Used in mods that swap textures without use of SURS
+            SkinnedMeshRenderer renderer = desiredObject.GetComponent<SkinnedMeshRenderer>();
+            if (renderer != null)
+            {
+                MelonLoader.MelonLogger.Msg("Texture Loaded");
+                Material material = renderer.material;
+                return material;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static Texture2D SURSImageCall(string filename)
+        {
+            //Used in SURS (Signalis Universal ReSkin Mod)
+            byte[] imageData = System.IO.File.ReadAllBytes(filename);
+            Texture2D SURStexture = new Texture2D(2, 2);
+            ImageConversion.LoadImage(SURStexture, imageData);
+            return SURStexture;
+        }
+
+    }
+}
