a8bc5c9ba807df0a35ae9c6fcccbd6104c37c2a5
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-03-27 15:45:41 -0400

    Added C Methods

---
+++ C Methods/CMathmaticalFunctions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+double average (int arr[])
+{
+    int length = array_length(arr);
+    int sum = 0;
+    double average;
+    for (int i = 0; i < length; i++)
+    {
+        sum += arr[i];
+    }
+    average = (double) sum / length;
+    return average;
+}
+
+int array_length (int arr[])
+{
+    int length = sizeof(arr) / sizeof(arr[0]);
+    return length;
+}
