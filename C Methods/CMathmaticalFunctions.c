94fdc2d91a012ff3678ecdd0c0b9f3b2b2f9653f
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-03-27 20:24:10 -0400

    Added Swap Command

--- C Methods/CMathmaticalFunctions.c
+++ C Methods/CMathmaticalFunctions.c
@@ -18,3 +18,10 @@ int array_length (int arr[])
     int length = sizeof(arr) / sizeof(arr[0]);
     return length;
 }
+void swap (int *num1, int *num2)
+{
+    int temp;
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
