41c25bd1b4675d9602e06708a6a10b38ad2b63e1
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-04-02 08:58:21 -0400

    Updated CSAI
    Simple Reponse based Chat Bot

---
+++ AI/C# AI Attempts/Interact.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var chatbot = new Chatbot();
+        Console.WriteLine("Type 'quit' to exit.");
+        while (true)
+        {
+            Console.Write("> ");
+            var input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+            var response = chatbot.Respond(input);
+            Console.WriteLine(response);
+        }
+    }
+}
--- AI/C# AI Attempts/VSL_CSAI.cs
+++ AI/C# AI Attempts/VSL_CSAI.cs
@@ -1,20 +1,22 @@
 ﻿public class Chatbot
 {
-    public string Name { get; set; }
-    public string[] Greetings { get; set; }
-    public string[] Farewells { get; set; }
-    public Dictionary<string, string> Responses { get; set; }
-    public Chatbot(string filePath)
+    private string[] Greetings = { "hi", "hello", "hey" };
+    private string[] Date = {"what is the date", "what is todays date", "what day is it today", "date"};
+    private string[] Ass = { "how big is your ass", "ass size when?", "ass?", "ass size?" };
+    public string Respond(string message)
     {
-        var lines = File.ReadAllLines(filePath);
-        Name = lines[0];
-        Greetings = lines[1].Split(',');
-        Farewells = lines[2].Split(',');
-        Responses = new Dictionary<string, string>();
-        for (int i = 3; i < lines.Length; i++)
+        if (Greetings.Contains(message.ToLower()))
         {
-            var parts = lines[i].Split('=');
-            Responses.Add(parts[0], parts[1]);
+            return "Hi there!";
         }
+        if (Date.Contains(message.ToLower()))
+        {
+            return $"Today's date is {DateTime.Now.ToString("MMMM dd, yyyy")}.";
+        }
+        if (Ass.Contains(message.ToLower()))
+        {
+            return "That is very crude";
+        }
+        return "I'm sorry, I don't understand.";
     }
 }
