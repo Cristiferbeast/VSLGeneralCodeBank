dde055de92d8fc2fd24eeb1d38edf42e4d3a217b
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-04-01 22:43:27 -0400

    Add files via upload

---
+++ AI/C# AI Attempts/VSL_CSAI.cs
@@ -0,0 +1,20 @@
+﻿public class Chatbot
+{
+    public string Name { get; set; }
+    public string[] Greetings { get; set; }
+    public string[] Farewells { get; set; }
+    public Dictionary<string, string> Responses { get; set; }
+    public Chatbot(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+        Name = lines[0];
+        Greetings = lines[1].Split(',');
+        Farewells = lines[2].Split(',');
+        Responses = new Dictionary<string, string>();
+        for (int i = 3; i < lines.Length; i++)
+        {
+            var parts = lines[i].Split('=');
+            Responses.Add(parts[0], parts[1]);
+        }
+    }
+}
