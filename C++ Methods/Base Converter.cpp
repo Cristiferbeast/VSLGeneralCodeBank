6612e7ee682cf1e75a82a719c16396b0e5bca0c5
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-04-18 23:51:49 -0400

    Add files via upload

---
+++ C++ Methods/Base Converter.cpp
@@ -0,0 +1,9 @@
+
+//This is just a vague write up of what it would look like, actual working code would require a bit of an edit
+
+int x = 27;
+int base =3; 
+int conversion (int x, int base){
+    new vector converted = {}
+
+}
