e20c88bdc564d5340dc198c61fa6f879e3d70485
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-05-12 20:17:09 -0400

    Update C++ Methods.cpp

--- C++ Methods/C++ Methods.cpp
+++ C++ Methods/C++ Methods.cpp
@@ -9,26 +9,6 @@ void waitenter() {
 	std::getline(std::cin, line);
 	std::cin.clear();
 }
-int getInt() {
-	int num;
-	std::string input;
-	bool isValid = false;
-
-	while (!isValid) {
-		std::getline(std::cin, input);
-
-		try {
-			num = std::stoi(input);
-			isValid = true;
-		}
-		catch (const std::invalid_argument& e) {
-			std::cout << "Invalid input. Please enter an integer." << std::endl;
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		}
-	}
-	return num;
-}
 static float CertainityRange(float participants, float percentage)
 {
     float temp;
