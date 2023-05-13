96264b16b814045b2a12f468556db1b84dae3681
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-05-13 11:00:22 -0400

    Update C# Methods.cs

--- C# Methods/C# Methods.cs
+++ C# Methods/C# Methods.cs
@@ -11,5 +11,10 @@ namespace VSLCSharpCodeBank
             }
             return (double)sum / arr.Length;
         }
+        public static double Circumference(int radius)
+        {
+            double Pi = 3.141592653589793238462643383279502884197; 
+            return Pi * 2 * radius;
+        }
     }
 }
