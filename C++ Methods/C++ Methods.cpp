af91fa0acbbd6e7e950e500554f8815144b37b98
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-05-28 14:13:09 -0400

    Add files via upload

--- C++ Methods/C++ Methods.cpp
+++ C++ Methods/C++ Methods.cpp
@@ -13,12 +13,34 @@ namespace VSLCodeBankCPP
     class Stat{
         static float CertainityRange(float participants, float percentage)
         {
-		float temp;
-		temp = 1-percentage;
-		temp *= percentage/100;
-		temp /= sqrt(participants);
-		temp *= 1.96f;
-		return temp;
-	}
+		    float temp;
+		    temp = 1-percentage;
+		    temp *= percentage/100;
+		    temp /= sqrt(participants);
+		    temp *= 1.96f;
+		    return temp;
+	    }
     }
+    class Conversions{
+        static int tempC(int F)
+        {
+            F -= 32;
+            F *= (5/9);
+            return F;
+        }
+        static int tempF(int C)
+        {
+            C *= (9/5);
+            C += 32;
+            return C;
+        }
+        static float UStoLei(float US)
+        {
+            return US *= 4.32;
+        }
+        static float LeitoUS(float Lei)
+        {
+            return Lei /= 4.32;
+        }
+    };
 ;}
