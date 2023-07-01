42c04c04a5f8e92dc22fd36f7139fad2060e58ad
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-07-01 14:29:28 -0400

    Add files via upload

---
+++ Swift Methods/Untitled-1.swift
@@ -0,0 +1,12 @@
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+   var currMin = array[0]
+   var currMax = array[0]
+   for value in array[1..<array.count] {
+      if value < currMin {
+        currMin = value
+      } else if value > currMax {
+         currMax = value
+      }
+   }
+  return (currMin, currMax)
+}
