44af955294aef6756a1bdb03ff01b7c0fe43a456
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-07-23 23:40:14 -0400

    Update SimpleJSMethods.js

--- JS Methods/SimpleJSMethods.js
+++ JS Methods/SimpleJSMethods.js
@@ -105,3 +105,11 @@ function graph(canvasid, data){
 	}
 
 }
+function shuffle(array) {
+    var _a;
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        _a = [array[j], array[i]], array[i] = _a[0], array[j] = _a[1];
+    }
+    return array;
+}
